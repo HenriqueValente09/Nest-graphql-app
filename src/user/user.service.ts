271fdcfb6eaaac4682681b0fc65dcf4f5c1358a8
HenriqueValente09 <valentedev09@gmail.com>
2023-02-28 21:31:42 -0300

    feat: user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository:Repository<User>
+    ){}
+
+    async findAllUsers(): Promise<User[]>{
+        const users = await this.userRepository.find()
+        return users;
+    }
+
+    async findUserById(id: string): Promise<User>{
+        const options = { relations: ['posts'] };
+        const user = await this.userRepository.findOneBy({id: id})
+        if(!user){
+            throw new NotFoundException("Usuário não encontrado!");
+            
+        }
+        return user;
+    }
+
+    async createUser(data: CreateUserInput): Promise<User> {
+        const user = this.userRepository.create(data)
+        const userSaved = await this.userRepository.save(user)
+
+        if(!userSaved){
+            throw new InternalServerErrorException('Problem para criar um usuário')
+        }
+
+        return userSaved;
+    }
+
+    async updateUser(id: string, data: UpdateUserInput): Promise<User> {
+        const user = await this.findUserById(id)
+        await this.userRepository.update(user, {...data})
+
+        const userUpdated = this.userRepository.create({...user, ...data})
+
+        if(!userUpdated){
+            throw new InternalServerErrorException('Problem para criar um usuário')
+        }
+
+        return userUpdated;
+    }
+
+    async deleteUser(id:string): Promise<boolean> {
+        const user = await this.findUserById(id)
+        const deleted = await this.userRepository.delete(user)
+
+        if(deleted) {
+            return true;
+        }
+
+        return false;
+    }
+}
