8894b311a34969c8ec1e5c1c161e48bd287bdae5
HenriqueValente09 <valentedev09@gmail.com>
2023-02-28 21:30:23 -0300

    feat: update-user dto

---
+++ src/user/dto/update-user.input.ts
@@ -0,0 +1,16 @@
+import { InputType } from "@nestjs/graphql"
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator"
+
+
+@InputType()
+export class UpdateUserInput {
+    @IsString()
+    @IsNotEmpty({message: "Este campo não pode estar vazio!"})
+    @IsOptional()
+    name:string
+
+    @IsEmail()
+    @IsNotEmpty({message: "Este campo não pode estar vazio!"})
+    @IsOptional()
+    email:string
+}
